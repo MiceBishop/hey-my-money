90ab7bab5050e0b0a2ed5589a34f14e837347d9c
Papa Seynou Sy Ndiaye <seynousy@gmail.com>
2017-09-25 23:48:35 +0000

    v1 of welcome step one screen

---
+++ js/components/StepOne.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  StyleSheet
+} from 'react-native'
+
+export default class StepOne extends Component {
+  handlePress() {
+
+  }
+
+  render() {
+    return(
+      <View style={styles.background}>
+        <View style={styles.part}>
+          <Text>Welcome</Text>
+        </View>
+        <View style={styles.part}>
+          <Image source={require('../../assets/images/logo-hey-my-money.png')} style={styles.logo} />
+        </View>
+        <View style={styles.part}>
+          <Button
+            title="Suivant"
+            color="#841584"
+            onPress={this.handlePress}
+            style={styles.nextButton}
+          />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  part: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    backgroundColor: "grey",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    height: 190,
+    width: 200,
+  }
+});
